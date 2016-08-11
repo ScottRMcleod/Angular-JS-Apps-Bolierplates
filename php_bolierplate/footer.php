b7f60b64787e2065a2fc339a70ae34d1b29dd8c7
Scott Mcleod <scott.mcleod@smcleodtech.com.au>
2016-08-11 17:32:03 +1000

    Update footer.php

--- php_bolierplate/footer.php
+++ php_bolierplate/footer.php
@@ -1,1 +1,41 @@
 
+ </div>
+
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-2">
+          <h4><a href='http://smcleodtech.com.au/pages/contact.php'>Contact Me</a></h4>
+                <br><font style='color:white'>
+                P: (04) 08392 896 <br>
+                E: <font style='color:white'><a a:visited style='color:white' href='mailto:scott.mcleod@smcleodtech.com.au' styles2>scott.mcleod@smcleodtech.com.au</a>
+                </font></p>
+        </div>
+        <div class="col-sm-4">
+            <a a:visited style='color:white' rel='nofollow external' title='Facebook' href='http://facebook.com/SmcleodTechnologies' target='_blank' class='btn btn-primary btn-circle'><i class='fa fa-facebook'> </i> </a>
+              <a a:visited style='color:white' rel='nofollow external' title='Linkedin' href='http://www.linkedin.com/in/scott-mcleod-ba658ba' target='_blank' class='btn btn-primary btn-circle'><i class='fa fa-linkedin'> </i> </a>
+              <a a:visited style='color:white' rel='nofollow external' title='twitter' href='https://twitter.com/06Mcleod' target='_blank' class='btn btn-primary btn-circle'><i class='fa fa-twitter'> </i> </a>
+              <a a:visited style='color:white' rel='nofollow external' title='GitHub' href='https://github.com/ScottRMcleod' target='_blank' class='btn btn-primary btn-circle'><i class='fa fa-github'> </i> </a>
+        </div>
+      </div>
+      <div class="col-sm-2">
+          <h4><a href='http://smcleodtech.com.au/pages/contact.php'>Contact Me</a></h4>
+                <br><font style='color:white'>
+                P: (04) 08392 896 <br>
+                E: <font style='color:white'><a a:visited style='color:white' href='mailto:scott.mcleod@smcleodtech.com.au' styles2>scott.mcleod@smcleodtech.com.au</a>
+                </font></p>
+        </div>
+        </div>
+  </footer>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+
+	<script src="js/jquery-1.11.2.min.js"></script>
+
+	<!-- Include all compiled plugins (below), or include individual files as needed --> 
+	<!-- <script src="js/bootstrap.js"></script> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="js/script.js"></script>
+</body>
+</html>
